0e9ac0e7e58cf4ab15c141ccfd8bf2cc876fc308
alexanderr <alexanderr@users.noreply.github.com>
2019-06-25 01:10:23 -0500

    init: Push accessories placement tool

---
+++ AccessoryPlacer.py
@@ -0,0 +1,428 @@
+HatModels = [None,
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_baseball',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_safari',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_ribbon',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_heart',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_topHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_anvil',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_flowerPot',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_sandbag',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_weight',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_fez',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_golfHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_partyHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_pillBox',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_crown',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_cowboyHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_pirateHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_propellerHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_fishingHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_sombreroHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_strawHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_sunHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_antenna',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_beeHiveHairdo',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_bowler',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_chefsHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_detective',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_feathers',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_fedora',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_mickeysBandConductorHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_nativeAmericanFeather',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_pompadorHairdo',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_princess',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_robinHoodHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_romanHelmet',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_spiderAntennaThingy',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_tiara',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_vikingHelmet',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_witch',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_wizard',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_conquistadorHelmet',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_firefighterHelmet',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_foilPyramid',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_minersHardhatWithLight',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_napoleonHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_pilotsCap',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_policeHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_rainbowAfroWig',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_sailorHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_carmenMirandaFruitHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_bobbyHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_jugheadHat',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_winter',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_bandana',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_dinosaur',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_band',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_birdNest',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_hat_mousekateer']
+HatTextures = [None,
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonRed.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonPurple.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_heartYellow.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_topHatBlue.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_safariBrown.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_safariGreen.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballBlue.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballOrange.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonYellow.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonChecker.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonLtRed.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonRainbow.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballYellow.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballRed.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballTeal.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonPinkDots.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_baseballPurple.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_ribbonCheckerGreen.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_partyToon.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_hat_bowlerRoger.jpg',
+ 'phase_4/maps/tt_m_chr_avt_acc_hat_mousekateer.jpg']
+GlassesModels = [None,
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_roundGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_miniblinds',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_narrowGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_starGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_3dGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_aviator',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_catEyeGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_dorkGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_jackieOShades',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_scubaMask',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_goggles',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_grouchoMarxEyebrow',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_heartGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_insectEyeGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_masqueradeTypeMask',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_masqueradeTypeMask3',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_monocle',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_mouthGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_squareRims',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_eyepatch',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_alienGlasses',
+ 'phase_4/models/accessories/tt_m_chr_avt_acc_msk_hypno_goggles17']
+GlassesTextures = [None,
+ 'phase_4/maps/tt_t_chr_avt_acc_msk_masqueradeTypeMask2.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_msk_masqueradeTypeMask4.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_msk_masqueradeTypeMask5.jpg',
+ 'phase_4/maps/tt_t_chr_avt_acc_msk_eyepatchGems.jpg']
+
+ 
+HeadBaseDir = 'phase_3'
+
+HeadDict = {
+    'dls': '/models/char/tt_a_chr_dgm_shorts_head_1000',
+    'dss': '/models/char/tt_a_chr_dgm_skirt_head_1000',
+    'dsl': '/models/char/tt_a_chr_dgs_shorts_head_1000',
+    'dll': '/models/char/tt_a_chr_dgl_shorts_head_1000',
+    'c': '/models/char/cat-heads-1000',
+    'h': '/models/char/horse-heads-1000',
+    'm': '/models/char/mouse-heads-1000',
+    'r': '/models/char/rabbit-heads-1000',
+    'f': '/models/char/duck-heads-1000',
+    'p': '/models/char/monkey-heads-1000',
+    'b': '/models/char/bear-heads-1000',
+    's': '/models/char/pig-heads-1000',
+    'u': '/models/char/hedgehog-heads-1000',
+    'k': '/models/char/kangaroo-heads-',
+}
+
+
+HeadSizes = [
+    'us',
+    'ul',
+    'ks',
+    'kl',
+    'cs',
+    'cl',
+    'ms',
+    'ml',
+    'rs',
+    'rl',
+    'fs',
+    'fl',
+    'ps',
+    'pl',
+    'bs',
+    'bl',
+    'ss',
+    'sl',
+]
+
+
+    
+
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData
+import json
+
+class AccessoryPlacer(ShowBase):
+ 
+    def __init__(self):
+        ShowBase.__init__(self)
+ 
+base = AccessoryPlacer()
+
+loadPrcFileData('', 'default-model-extension .bam')
+loadPrcFileData('', 'model-path ../resources')
+
+
+with open('accessories.json', 'r') as f:
+    data = json.load(f)
+
+
+currHeadIndex = 0
+currHead = None
+currHatIndex = 0
+currHat = None
+currGlassesIndex = 0
+currGlasses = None
+currHatPlacer = None
+currGlassesPlacer = None
+
+def loadHead():
+    global currHead, currHeadIndex
+    headSize = HeadSizes[currHeadIndex]
+    species, size = headSize
+    currHead = loader.loadModel(HeadBaseDir + HeadDict[species])
+    if size == 'l':
+        showLongHead(currHead)
+    elif size == 's':
+        showShortHead(currHead)
+
+    currHead.reparentTo(render)
+
+def unloadHead():
+    global currHead
+    currHead.removeNode()
+
+
+def changeHead(offset):
+    global currHeadIndex
+    currHeadIndex += offset
+    
+    if currHeadIndex >= len(HeadSizes):
+        currHeadIndex = 0
+    elif currHeadIndex <= -1:
+        currHeadIndex = len(HeadSizes) - 1
+    
+    unloadHead()
+    loadHead()
+    loadHat()
+    loadGlasses()
+
+
+def loadHat():
+    global currHeadIndex, currHatIndex, currHat, hatLabel
+    headSize = HeadSizes[currHeadIndex]
+    print('loadHat', currHatIndex)
+    if currHatIndex == 0:
+        hatLabel['text'] = 'Hat: None'
+        return
+    modelName = HatModels[currHatIndex]
+    hatLabel['text'] = 'Hat: ' + modelName
+    currHat = loader.loadModel(modelName)
+    try:
+        pos, hpr, scale = data['hats']['specific'][currHatIndex][headSize]
+        print(pos, hpr, scale)
+        currHat.setPos(*pos)
+        currHat.setHpr(*hpr)
+        currHat.setScale(*scale)
+    except KeyError:
+        try:
+            pos, hpr, scale = data['hats']['defaults'][headSize]
+            print(pos, hpr, scale)
+            currHat.setPos(*pos)
+            currHat.setHpr(*hpr)
+            currHat.setScale(*scale)
+        except KeyError:
+            pass
+    attachHat()
+
+
+def attachHat():
+    global currHat, currHead
+    hatNode = currHead.attachNewNode('hatNode')
+    currHat.reparentTo(hatNode)
+
+def changeHat(offset):
+    global currHatIndex, currHatPlacer
+    currHatIndex += offset
+    
+    if currHatIndex >= len(HatModels):
+        currHatIndex = 0
+    elif currHatIndex <= -1:
+        currHatIndex = len(HatModels) - 1
+    
+    unloadHat()
+    
+    if currHatPlacer:
+        currHatPlacer.destroy()
+        currHatPlacer = None
+    
+    loadHat()
+    
+def unloadHat():
+    global currHat
+    if not currHat:
+        return
+    currHat.removeNode()
+    currHat = None
+
+def loadGlasses():
+    global currHeadIndex, currGlassesIndex, currGlasses, glassesLabel
+    headSize = HeadSizes[currHeadIndex]
+    if currGlassesIndex == 0:
+        glassesLabel['text'] = 'Glasses: None' 
+        return
+    modelName = GlassesModels[currGlassesIndex]
+    glassesLabel['text'] = 'Hat: ' + modelName
+    currGlasses = loader.loadModel(modelName)
+    try:
+        pos, hpr, scale  = data['glasses']['specific'][currGlassesIndex][headSize]
+        currGlasses.setPos(*pos)
+        currGlasses.setHpr(*hpr)
+        currGlasses.setScale(*scale)
+    except KeyError:
+        try:
+            pos, hpr, scale = data['glasses']['defaults'][headSize]
+            currGlasses.setPos(*pos)
+            currGlasses.setHpr(*hpr)
+            currGlasses.setScale(*scale)
+        except KeyError:
+            pass
+    attachGlasses()
+
+    
+def attachGlasses():
+    global currGlasses, currHead
+    glassesNode = currHead.attachNewNode('glassesNode')
+    currGlasses.reparentTo(glassesNode)
+
+def changeGlasses(offset):
+    global currGlassesIndex, currGlassesPlacer
+    currGlassesIndex += offset
+    
+    if currGlassesIndex >= len(GlassesModels):
+        currGlassesIndex = 0
+    elif currGlassesIndex <= -1:
+        currGlassesIndex = len(GlassesModels) - 1
+    
+    unloadGlasses()
+    if currGlassesPlacer:
+        currGlassesPlacer.destroy()
+        currGlassesPlacer = None
+    loadGlasses()
+
+
+def unloadGlasses():
+    global currGlasses
+    if not currGlasses:
+        return
+    currGlasses.removeNode()
+    currGlasses = None
+
+
+def showLongHead(head):
+    head.findAllMatches('**/*-short*').hide()
+    head.findAllMatches('**/*_short*').hide()
+    head.findAllMatches('**/muzzle-long-*').hide()
+    head.ls()
+    muzzle = head.find('**/muzzle-long-neutral')
+    if muzzle and not muzzle.isEmpty():
+        muzzle.show()
+    else:
+        muzzle = head.find('**/muzzle-short-neutral')
+        muzzle.show()
+
+def showShortHead(head):
+    head.findAllMatches('**/*-long*').hide()
+    head.findAllMatches('**/*_long*').hide()
+    head.findAllMatches('**/muzzle-short-*').hide()
+    head.ls()
+    head.find('**/muzzle-short-neutral').show()
+    
+
+def save():
+    global currHeadIndex, currGlassesIndex, currHatIndex, currGlasses, currHat
+    headSize = HeadSizes[currHeadIndex]
+    if currGlassesIndex and currGlasses:
+        pos, hpr, scale = list(currGlasses.getPos()), list(currGlasses.getHpr()), list(currGlasses.getScale())
+        if currGlassesIndex not in data['glasses']['specific']:
+            data['glasses']['specific'][currGlassesIndex] = dict()
+        data['glasses']['specific'][currGlassesIndex][headSize] = [pos, hpr, scale]
+
+    if currHatIndex and currHat:
+        pos, hpr, scale = list(currHat.getPos()), list(currHat.getHpr()), list(currHat.getScale())
+        if currHatIndex not in data['hats']['specific']:
+            data['hats']['specific'][currHatIndex] = dict()
+        data['hats']['specific'][currHatIndex][headSize] = [pos, hpr, scale]
+
+    with open('accessories.json', 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=2, separators=(',', ': '))
+
+from direct.gui.DirectGui import *
+from panda3d.core import TextNode
+
+
+DGG.setDefaultFont(loader.loadFont('phase_3/models/fonts/ImpressBT.ttf'))
+DGG.setDefaultRolloverSound(loader.loadSfx('phase_3/audio/sfx/GUI_rollover.ogg'))
+DGG.setDefaultClickSound(loader.loadSfx('phase_3/audio/sfx/GUI_create_toon_fwd.ogg'))
+DGG.setDefaultDialogGeom(loader.loadModel('phase_3/models/gui/dialog_box_gui'))
+
+frame = DirectFrame(parent=base.a2dTopRight,relief=DGG.SUNKEN, borderWidth=(0.01, 0.01), frameSize=(-0.3, 0.3, -0.3, 0.3), pos=(-0.3, 0, -0.3))
+nextHead = DirectButton(parent=frame, relief=2, text='Next Head', text_scale=0.04, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(0.15, 0, 0.2), command=changeHead, extraArgs=[1])
+previousHead = DirectButton(parent=frame, relief=2, text='Prev Head', text_scale=0.04, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(-0.15, 0, 0.2), command=changeHead, extraArgs=[-1])
+nextHat = DirectButton(parent=frame, relief=2, text='Next Hat', text_scale=0.04, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(0.15, 0, 0.05), command=changeHat, extraArgs=[1])
+previousHat = DirectButton(parent=frame, relief=2, text='Prev Hat', text_scale=0.04, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(-0.15, 0, 0.05), command=changeHat, extraArgs=[-1])
+nextGlasses = DirectButton(parent=frame, relief=2, text='Next Glasses', text_scale=0.035, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(0.15, 0, -0.05), command=changeGlasses, extraArgs=[1])
+previousGlasses = DirectButton(parent=frame, relief=2, text='Prev Glasses', text_scale=0.035, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(-0.15, 0, -0.05), command=changeGlasses, extraArgs=[-1])
+
+saveButton = DirectButton(parent=frame, relief=2, text='Save', text_scale=0.035, borderWidth=(0.01, 0.01), frameSize=(-0.1, 0.1, -0.05, 0.05), pos=(-0.15, 0, -0.25), command=save)
+
+hatLabel = DirectLabel(parent=base.a2dBottomCenter, relief=None, text='Hat:', text_scale=0.05, pos=(0, 0, 0.1), text_align=TextNode.ACenter)
+glassesLabel = DirectLabel(parent=base.a2dBottomCenter, relief=None, text='Glasses:', text_scale=0.05, pos=(0, 0, .2), text_align=TextNode.ACenter)
+
+from PlacerTool3D import PlacerTool3D
+from panda3d.core import Point3, Quat
+
+def placeHat():
+    global currHatPlacer
+    if currHat:
+        if currHatPlacer:
+            currHatPlacer.destroy()
+        currHatPlacer = PlacerTool3D(currHat)
+
+def placeGlasses():
+    global currGlassesPlacer
+    if currGlasses:
+        if currGlassesPlacer:
+            currGlassesPlacer.destroy()
+        currGlassesPlacer = PlacerTool3D(currGlasses)
+
+cameraLerps = [
+    (Point3(0, 5, 0), Point3(180, 0, 0),),
+    (Point3(-5, 0, 0), Point3(270, 0, 0),),
+    (Point3(0, -5, 0), Point3(0, 0, 0),),
+    (Point3(5, 0, 0), Point3(90, 0, 0),),
+]
+
+def cameraLerp(i):
+    pos, hpr = cameraLerps[i]
+    quat = Quat()
+    quat.setHpr(hpr)
+    camera.posQuatInterval(1.2, pos, quat).start()
+
+
+base.accept('h', placeHat)
+base.accept('g', placeGlasses)
+base.accept('o', base.oobe)
+base.accept('1', lambda: cameraLerp(1))
+base.accept('2', lambda: cameraLerp(2))
+base.accept('3', lambda: cameraLerp(3))
+
+loadHead()
+
+camera.setPosHpr(0, 5, 0, 180, 0, 0)
+base.disableMouse()
+base.run()
